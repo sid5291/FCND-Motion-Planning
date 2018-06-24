464f9dfd846ea0aa8e7670acfb46748ae1267f76
sid.kumar <sid.kumar@airbus-sv.com>
2018-06-23 22:29:32 -0700

    Add debug

--- motion_planning.py
+++ motion_planning.py
@@ -368,8 +368,8 @@ class MotionPlanning(Drone):
         # TODO (if you're feeling ambitious): Try a different approach altogether!
         # Convert path to waypoints
         waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
-        graph_waypoints = [[p[0], p[1], TARGET_ALTITUDE, 0] for p in graph_path]
-        for  point in waypoints:
+        graph_waypoints = [list(self.convert_to_int([p[0], p[1], TARGET_ALTITUDE, 0])) for p in graph_path]
+        for  point in graph_waypoints:
             print("Point: {0}".format(point))
         # Set self.waypoints
         self.waypoints = graph_waypoints
