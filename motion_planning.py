14227665c222f6854a7f913fb7eb4cbe766cbb4a
sid.kumar <sid.kumar@airbus-sv.com>
2018-06-23 22:12:58 -0700

    Add debug

--- motion_planning.py
+++ motion_planning.py
@@ -365,8 +365,7 @@ class MotionPlanning(Drone):
         # Convert path to waypoints
         waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
         graph_waypoints = [[p[0], p[1], TARGET_ALTITUDE, 0] for p in graph_path]
-        for point in graph_waypoints:
-            print("Point: {0}".format(point))
+        print("Points: {0}".format(len(graph_waypoints)))
         # Set self.waypoints
         self.waypoints = waypoints
         # TODO: send waypoints to sim (this is just for visualization of waypoints)
